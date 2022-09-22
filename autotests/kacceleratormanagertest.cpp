8981ef1fb1804a217949a42da6dc7c5ac20c37bd
Deepin Developer <deepin-dev@deepin.org>
2022-09-22 17:43:20 +0800

    feat: Init commit
    init kwidgetsaddons
    
    Log:

---
+++ autotests/kacceleratormanagertest.cpp
@@ -0,0 +1,109 @@
+/*
+    This file is part of the KDE libraries
+    SPDX-FileCopyrightText: 2017 David Faure <faure@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+#include <functional>
+#include <kacceleratormanager.h>
+
+#include <QMenu>
+#include <QTest>
+
+#define QSL QStringLiteral
+
+static QStringList extractActionTexts(QMenu &menu, std::function<QString(const QAction *)> func)
+{
+    menu.aboutToShow(); // signals are now public in Qt5, how convenient :-)
+
+    QStringList ret;
+    bool lastIsSeparator = false;
+    const auto menuActions = menu.actions();
+    for (const QAction *action : menuActions) {
+        if (action->isSeparator()) {
+            if (!lastIsSeparator) { // Qt gets rid of duplicate separators, so we should too
+                ret.append(QStringLiteral("separator"));
+            }
+            lastIsSeparator = true;
+        } else {
+            lastIsSeparator = false;
+            if (action->menu()) {
+                ret.append(QStringLiteral("submenu"));
+            } else {
+                const QString text = func(action);
+                ret.append(text);
+            }
+        }
+    }
+    return ret;
+}
+
+class KAcceleratorManagerTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void initTestCase()
+    {
+    }
+
+    void testActionTexts_data()
+    {
+        QTest::addColumn<QStringList>("initialTexts");
+        QTest::addColumn<QStringList>("expectedTexts");
+
+        QTest::newRow("basic") << QStringList{QSL("Open"), QSL("Close"), QSL("Quit")} << QStringList{QSL("&Open"), QSL("&Close"), QSL("&Quit")};
+        QTest::newRow("same_first") << QStringList{QSL("Open"), QSL("Close"), QSL("Clone"), QSL("Quit")}
+                                    << QStringList{QSL("&Open"), QSL("&Close"), QSL("C&lone"), QSL("&Quit")};
+        QTest::newRow("cjk") << QStringList{QSL("Open (&O)"), QSL("Close (&C)"), QSL("Clone (&C)"), QSL("Quit (&Q)")}
+                             << QStringList{QSL("Open (&O)"), QSL("Close (&C)"), QSL("C&lone (C)"), QSL("Quit (&Q)")};
+    }
+
+    void testActionTexts()
+    {
+        // GIVEN
+        QFETCH(QStringList, initialTexts);
+        QFETCH(QStringList, expectedTexts);
+        QMenu menu;
+        for (const QString &text : std::as_const(initialTexts)) {
+            menu.addAction(text);
+        }
+        // WHEN
+        KAcceleratorManager::manage(&menu);
+        // THEN
+        const QStringList texts = extractActionTexts(menu, &QAction::text);
+        QCOMPARE(texts, expectedTexts);
+    }
+
+    void testActionIconTexts_data()
+    {
+        QTest::addColumn<QStringList>("initialTexts");
+        QTest::addColumn<QStringList>("expectedTexts");
+
+        QTest::newRow("basic") << QStringList{QSL("Open"), QSL("Close"), QSL("Quit")} << QStringList{QSL("Open"), QSL("Close"), QSL("Quit")};
+        QTest::newRow("same_first") << QStringList{QSL("Open"), QSL("Close"), QSL("Clone"), QSL("Quit")}
+                                    << QStringList{QSL("Open"), QSL("Close"), QSL("Clone"), QSL("Quit")};
+        QTest::newRow("cjk") << QStringList{QSL("Open (&O)"), QSL("Close (&C)"), QSL("Clone (&C)"), QSL("Quit (&Q)")}
+                             << QStringList{QSL("Open"), QSL("Close"), QSL("Clone"), QSL("Quit")};
+    }
+
+    void testActionIconTexts()
+    {
+        // GIVEN
+        QFETCH(QStringList, initialTexts);
+        QFETCH(QStringList, expectedTexts);
+        QMenu menu;
+        for (const QString &text : std::as_const(initialTexts)) {
+            menu.addAction(text);
+        }
+        // WHEN
+        KAcceleratorManager::manage(&menu);
+        // THEN
+        const QStringList iconTexts = extractActionTexts(menu, &QAction::iconText);
+        QCOMPARE(iconTexts, expectedTexts);
+    }
+};
+
+QTEST_MAIN(KAcceleratorManagerTest)
+
+#include "kacceleratormanagertest.moc"
---
+++ src/kmimetypeeditor.cpp
@@ -0,0 +1,34 @@
+/*
+    This file is part of the KDE libraries
+    SPDX-FileCopyrightText: 2014 David Faure <faure@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#include "kmimetypeeditor.h"
+#include "kmessagedialog.h"
+
+#include <QObject>
+#include <QProcess>
+
+static const char s_keditfiletypeExecutable[] = "keditfiletype5";
+
+void KMimeTypeEditor::editMimeType(const QString &mimeType, QWidget *widget)
+{
+    QStringList args;
+#ifndef Q_OS_WIN
+    args << QStringLiteral("--parent") << QString::number(widget->window()->winId());
+#endif
+    args << mimeType;
+
+    const bool result = QProcess::startDetached(QString::fromLatin1(s_keditfiletypeExecutable), args);
+    if (!result) {
+        auto *dlg = new KMessageDialog(KMessageDialog::Error,
+                                       QObject::tr("Could not start the \"keditfiletype5\" executable, please check your installation."),
+                                       widget);
+        dlg->setAttribute(Qt::WA_DeleteOnClose);
+        dlg->setModal(true);
+        dlg->setButtons();
+        dlg->show();
+    }
+}
